079fdeb45f2768ef9f0468398ce32b87ed7eca66
kod2nd <colinongengkiat@gmail.com>
2018-07-25 23:11:07 +0800

    renamed getNearestArea.js variables

--- src/getNearestArea.js
+++ src/getNearestArea.js
@@ -1,14 +1,14 @@
 import {distanceCalculator} from './distanceCalculator'
 
 const getNearestArea = (nowCast, userLat, userLng) => {
-    let areaStoring = Number.POSITIVE_INFINITY
+    let closestWeatherStation = Number.POSITIVE_INFINITY
     let closestAreaToStation
 
     nowCast.forEach((location) => {
-        const dist = distanceCalculator(location.label_location.latitude, location.label_location.longitude, userLat, userLng)
+        const distanceFromWeatherStation = distanceCalculator(location.label_location.latitude, location.label_location.longitude, userLat, userLng)
 
-        if (dist < areaStoring) {
-            areaStoring = dist
+        if (distanceFromWeatherStation < closestWeatherStation) {
+            closestWeatherStation = distanceFromWeatherStation
             closestAreaToStation = location.name
         }
     })
