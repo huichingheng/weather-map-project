6f5094d56a92063a2506873d0edaa96337d471ba
kod2nd <colinongengkiat@gmail.com>
2018-07-25 23:01:35 +0800

    renamed distanceCalculator variables and updated test

--- src/distanceCalculator.js
+++ src/distanceCalculator.js
@@ -1,19 +1,19 @@
 
-const deg2rad = (deg) => {
+const degreesToRadians = (deg) => {
     return deg * (Math.PI/180)
   }
 
 export const distanceCalculator = (lat1, lng1, lat2, lng2) => {
-        var R = 6371; // Radius of the earth in km
-        var dLat = deg2rad(lat2-lat1);  // deg2rad below
-        var dLon = deg2rad(lng2-lng1); 
-        var a = 
+        const EARTH_RADIUS = 6371;
+        const dLat = degreesToRadians(lat2-lat1);  // deg2rad below
+        const dLon = degreesToRadians(lng2-lng1); 
+        const a = 
           Math.sin(dLat/2) * Math.sin(dLat/2) +
-          Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+          Math.cos(degreesToRadians(lat1)) * Math.cos(degreesToRadians(lat2)) * 
           Math.sin(dLon/2) * Math.sin(dLon/2)
           ; 
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-        var d = R * c; // Distance in km
-        return d;
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+        const distanceInKM = EARTH_RADIUS * c;
+        return distanceInKM;
       
 }
