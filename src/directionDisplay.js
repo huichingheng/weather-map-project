fe476625307cdaf7a77bf08e20fc08f95906b0dd
kod2nd <colinongengkiat@gmail.com>
2018-07-25 22:49:35 +0800

    refactored directionDisplay function

--- src/directionDisplay.js
+++ src/directionDisplay.js
@@ -1,21 +1,16 @@
+const directionCalculator = (bearing, angle, offset = 45) => {
+      // Start angle starts at North East (22.5 degrees)
+      const startAngle = 360 / 16 
+      return (bearing - (startAngle + angle)) * (bearing - (startAngle + angle + offset))
+}
+
 export const directionDisplay = (bearing) => {
-    
-    const angleAdder = [0, 45, 90, 135, 180, 225, 270, 315, 360]
-    const startAngle = 22.5
-    if ((bearing - (startAngle + angleAdder[0])) * (bearing - (startAngle + angleAdder[1])) <= 0) {
-        return "NE"}
-    if ((bearing - (startAngle + angleAdder[1])) * (bearing - (startAngle + angleAdder[2])) <= 0) {
-        return "E"}
-  if ((bearing - (startAngle + angleAdder[2])) * (bearing - (startAngle + angleAdder[3])) <= 0) {
-        return "SE"}
-  if ((bearing - (startAngle + angleAdder[3])) * (bearing - (startAngle + angleAdder[4])) <= 0) {
-        return "S"}
-  if ((bearing - (startAngle + angleAdder[4])) * (bearing - (startAngle + angleAdder[5])) <= 0) {
-        return "SW"}
-  if ((bearing - (startAngle + angleAdder[5])) * (bearing - (startAngle + angleAdder[6])) <= 0) {
-        return "W"}
-  if ((bearing - (startAngle + angleAdder[6])) * (bearing - (startAngle + angleAdder[7])) <= 0) {
-        return "NW"}
-  if ((bearing - (startAngle + angleAdder[7])) * (bearing - angleAdder[8]) <= 0) {
-        return "N"} else return "N"
+      if (directionCalculator(bearing, 0) <= 0) return "NE"
+      if (directionCalculator(bearing, 45) <= 0) return "E"
+      if (directionCalculator(bearing, 90) <= 0) return "SE"
+      if (directionCalculator(bearing, 135) <= 0) return "S"
+      if (directionCalculator(bearing, 180) <= 0) return "SW"
+      if (directionCalculator(bearing, 225) <= 0) return "W"
+      if (directionCalculator(bearing, 270) <= 0) return "NW"
+      return "N"
 }
