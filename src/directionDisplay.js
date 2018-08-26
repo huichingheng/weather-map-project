f3d7a9b2c70ae815b9526ca7527b8baadb8730b8
kod2nd <colinongengkiat@gmail.com>
2018-08-26 12:52:06 +0800

    moved compass magic string into object

--- src/directionDisplay.js
+++ src/directionDisplay.js
@@ -1,3 +1,14 @@
+const compass = {
+      NORTH: "N",
+      NORTH_EAST: "NE",
+      EAST: "E",
+      SOUTH_EAST: "SE",
+      SOUTH: "S",
+      SOUTH_WEST: "SW",
+      WEST: "W",
+      NORTH_WEST: "NW"
+}
+
 const directionCalculator = (bearing, angle, offset = 45) => {
       // Start angle starts at North East (22.5 degrees)
       const startAngle = 360 / 16 
@@ -5,12 +16,12 @@ const directionCalculator = (bearing, angle, offset = 45) => {
 }
 
 export const directionDisplay = (bearing) => {
-      if (directionCalculator(bearing, 0) <= 0) return "NE"
-      if (directionCalculator(bearing, 45) <= 0) return "E"
-      if (directionCalculator(bearing, 90) <= 0) return "SE"
-      if (directionCalculator(bearing, 135) <= 0) return "S"
-      if (directionCalculator(bearing, 180) <= 0) return "SW"
-      if (directionCalculator(bearing, 225) <= 0) return "W"
-      if (directionCalculator(bearing, 270) <= 0) return "NW"
-      return "N"
+      if (directionCalculator(bearing, 0) <= 0) return compass.NORTH_EAST
+      if (directionCalculator(bearing, 45) <= 0) return compass.EAST
+      if (directionCalculator(bearing, 90) <= 0) return compass.SOUTH_EAST
+      if (directionCalculator(bearing, 135) <= 0) return compass.SOUTH
+      if (directionCalculator(bearing, 180) <= 0) return compass.SOUTH_WEST
+      if (directionCalculator(bearing, 225) <= 0) return compass.WEST
+      if (directionCalculator(bearing, 270) <= 0) return compass.NORTH_WEST
+      return compass.NORTH
 }
